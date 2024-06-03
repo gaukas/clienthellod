c64cfd534c6f7e581239c424cbd9c62629bcc97d
Gaukas Wang <i@gaukas.wang>
2024-06-03 09:36:05 -0600

    fix: combine append call for performance
    Signed-off-by: Gaukas Wang <i@gaukas.wang>

--- internal/utils/typeconv.go
+++ internal/utils/typeconv.go
@@ -3,8 +3,7 @@ package utils
 func Uint16ToUint8(a []uint16) []uint8 {
 	b := make([]uint8, 0)
 	for _, v := range a {
-		b = append(b, uint8(v>>8))
-		b = append(b, uint8(v))
+		b = append(b, uint8(v>>8), uint8(v))
 	}
 	return b
 }
